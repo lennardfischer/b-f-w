17ee622622e192b5c716b002bd9225d4cecc5974
Richard Attermeyer <richard.attermeyer@opitz-consulting.com>
2019-11-13 08:36:43 +0100

    run docker on fedora31

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 sudo dnf update -y
 sudo dnf install -y ansible
+sudo dnf install grubby
+sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
 exec ./provision.sh
 
